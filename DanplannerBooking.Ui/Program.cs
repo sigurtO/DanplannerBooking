9fe7ba29861be2a02f74e61524e1cbcb3a16bb79
SigurtO <sigurt.olsen@hotmail.com>
2025-11-03 22:46:47 +0100

    added api base url

--- DanplannerBooking.Ui/Program.cs
+++ DanplannerBooking.Ui/Program.cs
@@ -6,6 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5001") // Your API base URL
+});
 
 await builder.Build().RunAsync();
