0c63d6437ed68f799831e71b3d47421e52dd4c2f
EmilUtoft <euvi67732@edu.ucl.dk>
2025-12-04 11:02:22 +0100

    Booking for space now available

--- DanplannerBooking.Infrastructure/Migrations/20251204093432_InitialCreate.Designer.cs
+++ DanplannerBooking.Infrastructure/Migrations/20251204093432_InitialCreate.Designer.cs
@@ -12,11 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DanplannerBooking.Infrastructure.Migrations
 {
     [DbContext(typeof(DbContextBooking))]
-<<<<<<<< HEAD:DanplannerBooking.Infrastructure/Migrations/20251203101942_InitialCreate.Designer.cs
-    [Migration("20251203101942_InitialCreate")]
-========
-    [Migration("20251203092019_InitialCreate")]
->>>>>>>> master:DanplannerBooking.Infrastructure/Migrations/20251203092019_InitialCreate.Designer.cs
+    [Migration("20251204093432_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
