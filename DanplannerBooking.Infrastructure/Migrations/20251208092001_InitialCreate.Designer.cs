d78b8f7a6914c37f2d962e888cb9255711daa92d
TgTweek <lukassandersen@gmail.com>
2025-12-08 11:36:16 +0100

    new dashboard

--- DanplannerBooking.Infrastructure/Migrations/20251208092001_InitialCreate.Designer.cs
+++ DanplannerBooking.Infrastructure/Migrations/20251208092001_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DanplannerBooking.Infrastructure.Migrations
 {
     [DbContext(typeof(DbContextBooking))]
-    [Migration("20251204093432_InitialCreate")]
+    [Migration("20251208092001_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
